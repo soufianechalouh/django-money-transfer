277539366fc41e9fdb981c9149f8de8a331beb0c
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-29 19:54:52 +0100

    Feature: Created default serializer and viewset for submissions

--- app/submissions/views.py
+++ app/submissions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import SubmissionSerializer
+from .models import Submission
 
-# Create your views here.
+
+class SubmissionViewset(viewsets.ModelViewSet):
+    serializer_class = SubmissionSerializer
+    queryset = Submission.objects.all()
