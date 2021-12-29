46ad0dc62c5b3f1f052df6e35669e3a813d313c1
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-29 14:55:22 +0100

    Feature: create submission model

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users',
+    'submissions',
 ]
 
 REST_FRAMEWORK = {
--- app/submissions/models.py
+++ app/submissions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Submission(models.Model):
+    SUBMISSION_STATUS_CHOICES = [(x, x) for x in ["DRAFT", "SUBMITTED", "IN_REVIEW", "VALIDATED", "DECLINED"]]
+    ID_TYPE_CHOICES = [(x, x) for x in ["ID_CARD", "DRIVER_LICENSE", "PASSPORT"]]
+
+    user = models.ForeignKey(User, related_name="submission", on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(User, limit_choices_to={'is_staff': True}, null=True,
+                                 related_name="submissions", on_delete=models.CASCADE)
+    status = models.CharField(max_length=32, choices=SUBMISSION_STATUS_CHOICES)
+    id_document = models.ImageField()
+    selfie = models.ImageField()
+    document_type = models.CharField(max_length=32, choices=ID_TYPE_CHOICES)
+    review = models.CharField(max_length=512, null=True, blank=True)
