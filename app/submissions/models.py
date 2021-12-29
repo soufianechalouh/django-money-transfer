ac21bf29706b216c86b60395fe207afbd1a41b93
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-29 15:20:50 +0100

    Feature: Add signal to set user as verified on submission

--- app/submissions/models.py
+++ app/submissions/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 from users.models import User
 
+from django.db.models.signals import post_save
+
 
 class Submission(models.Model):
-    SUBMISSION_STATUS_CHOICES = [(x, x) for x in ["DRAFT", "SUBMITTED", "IN_REVIEW", "VALIDATED", "DECLINED"]]
+    DRAFT = 'DR'
+    SUBMITTED = 'SU'
+    IN_REVIEW = 'IR'
+    VERIFIED = 'VF'
+    ESCALATED = 'ES'
+    DECLINED = 'DC'
+    SUBMISSION_STATUS_CHOICES = [
+        (DRAFT, 'DRAFT'),
+        (SUBMITTED, 'SUBMITTED'),
+        (IN_REVIEW, 'IN_REVIEW'),
+        (VERIFIED, 'VERIFIED'),
+        (ESCALATED, 'ESCALATED'),
+        (DECLINED, 'DECLINED'),
+    ]
     ID_TYPE_CHOICES = [(x, x) for x in ["ID_CARD", "DRIVER_LICENSE", "PASSPORT"]]
 
     user = models.ForeignKey(User, related_name="submission", on_delete=models.CASCADE)
@@ -15,3 +30,13 @@ class Submission(models.Model):
     selfie = models.ImageField()
     document_type = models.CharField(max_length=32, choices=ID_TYPE_CHOICES)
     review = models.CharField(max_length=512, null=True, blank=True)
+
+
+def submission_post_save(sender, instance, created,  *args, **kwargs):
+    if instance.status == Submission.VERIFIED and instance.user.is_verified is False:
+        user = instance.user
+        user.is_verified = True
+        user.save()
+
+
+post_save.connect(submission_post_save, sender=Submission)
