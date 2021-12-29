4c5f04261f86689ec355219cf7bc983ecdbca674
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-29 19:26:41 +0100

    Feature: Created serializers and viewsets for wallets and transfers

--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from wallet.models import Wallet, Transfer
+from wallet.serializers import WalletSerializer, TransferSerializer
+
+
+# TODO (soufiane): set permissions and restrict access
+class WalletViewset(viewsets.ModelViewSet):
+    serializer_class = WalletSerializer
+    queryset = Wallet.objects.all()
+
+
+class TransferViewset(viewsets.ModelViewSet):
+    serializer_class = TransferSerializer
+    queryset = Transfer.objects.all()
