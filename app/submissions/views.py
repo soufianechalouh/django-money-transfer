2f258f5afdd96e5a69f6019f0dd774b3f97d774a
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-30 14:39:19 +0100

    Feature: Created viewset for non_staff users and linked it with router

--- app/submissions/views.py
+++ app/submissions/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import SubmissionSerializer
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import SubmissionListCreateSerializer, SubmissionSerializer
 from .models import Submission
+from rest_framework.permissions import IsAuthenticated
 
 
-class SubmissionViewset(viewsets.ModelViewSet):
+class SubmissionViewset(ModelViewSet):
     serializer_class = SubmissionSerializer
     queryset = Submission.objects.all()
+
+
+class SubmissionListViewset(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = SubmissionListCreateSerializer
+    lookup_field = "id"
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Submission.objects.all()
+        return Submission.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
