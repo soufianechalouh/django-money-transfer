371412984bffa33f8d9fe23aab67557d1991618e
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-30 12:00:51 +0100

    Feature: Created views for registration, email validation, and login

--- app/app/settings.py
+++ app/app/settings.py
@@ -144,3 +144,10 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "host@email.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "******")
