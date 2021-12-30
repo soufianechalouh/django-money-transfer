fa23dfc8a518cc29d6cd1f8dcd9ca8506dd64572
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-30 18:24:15 +0100

    Feature: Customize wallets and transaction views to restrict access scope

--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,15 +1,37 @@
-from rest_framework import viewsets
+from django.db.models import Q
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
+from users.models import User
 from wallet.models import Wallet, Transfer
 from wallet.serializers import WalletSerializer, TransferSerializer
 
 
-# TODO (soufiane): set permissions and restrict access
-class WalletViewset(viewsets.ModelViewSet):
+class WalletViewset(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = WalletSerializer
-    queryset = Wallet.objects.all()
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Wallet.objects.all()
+        return Wallet.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class TransferViewset(viewsets.ModelViewSet):
+class TransferViewset(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = TransferSerializer
-    queryset = Transfer.objects.all()
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Transfer.objects.all()
+        return Transfer.objects.filter(Q(source__owner=self.request.user) | Q(destination__owner=self.request.user))
+
+    def perform_create(self, serializer):
+        if not User.objects.count(username=serializer.validated_data["destination"]["username"]):
+            raise ValueError("Destination user does not exist")
+        destination = User.objects.get(username=serializer.validated_data["destination"]["username"])
+        serializer.save(source=self.request.user, destination=destination)
