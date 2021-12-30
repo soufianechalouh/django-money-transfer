69e08971aa0ae5e18dc0c880df3faf6812c0ff8e
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-12-30 18:21:30 +0100

    Feature: Restrict write fields for wallet and transaction serializers

--- app/wallet/serializers.py
+++ app/wallet/serializers.py
@@ -7,11 +7,14 @@ class WalletSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Wallet
-        fields = "__all__"
+        read_only_fields = ("balance", "status")
 
 
 class TransferSerializer(serializers.ModelSerializer):
+    sender = serializers.CharField(source="source.username", read_only=True)
+    receiver = serializers.CharField(source="destination.username")
 
     class Meta:
         model = Transfer
-        fields = "__all__"
+        fields = ["sender", "receiver", "created_at", "amount", "status"]
+        read_only_fields = ("sender", "created_at", "status")
