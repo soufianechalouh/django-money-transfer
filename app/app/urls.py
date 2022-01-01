7d724fbd19e4df51acd7a8bb9abc68099c578a5e
Soufiane CHALOUH <soufianechalouh@gmail.com>
2022-01-01 11:14:17 +0100

    Feature: added bank ops serializers and viewsets, and linked them with main project

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("wallets/", include("wallet.urls")),
     path("submissions/", include("submissions.urls")),
+    path("bankops/", include("bankops.urls")),
     path("auth/", include("users.urls"))
 ]
 
